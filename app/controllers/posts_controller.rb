146f37e634cae4ecb443eff4dafe0d9f66a78d39
Pranav <pranau_b@s00015.local>
2014-04-08 15:47:40 +1000

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  
+  #before_filter :authenicate_user!, only: [:new, :create, :destroy]
+    
+      
+  def new
+    @post = Post.new
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to posts_path
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+    #@post = Post.new(post_params)
+ 
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+    
+  
+end
